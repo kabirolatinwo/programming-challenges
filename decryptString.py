0df29a592eb65c7cfc6c9c67317f832c83bcf539
kabirolatinwo <kabirolatinwo@gmail.com>
2020-01-07 22:49:56 -0500

    finished implementing solution with test cases

--- decryptString.py
+++ decryptString.py
@@ -38,24 +38,70 @@ s will be valid string such that mapping is always possible.
 """
 
 """
-TODO:
-	- add comments explaining thought process behind solution
-	- test cases to prove solution is correct and validate robustness against several different cases
+function that converts digits to characters
+KNOWN LIMITATIONS - solution can be optimized
 """
-class Solution:
-    def freqAlphabets(self, s: str) -> str:
-        decrypted_string = ""
-        count = 0
-        for i in range(len(s)):
-            if (s[i] == '#'):
-                for x in range(count, (i-2)):	#
-                    decrypted_string += chr(int(s[x])+96)
-                decrypted_string += chr(int(s[i-2:i])+ 96)
-                count = i+1
-            else:
-                continue
-        if (count-1 != len(s)):
-            for i in range(count, len(s)):
-                decrypted_string += chr(int(s[i])+96)
-        print(decrypted_string)
-        return decrypted_string
+def freqAlphabets(s):
+    if (type(s) != str):
+        return "Invalid Input"
+    decrypted_string = ""
+    last_hash_pos = 0
+    for i in range(len(s)):
+        #if we get to a #, convert all digits from last # pos until you get to the two digits
+        #   right before the #
+        #Then concatenate the two digits and convert to the respective character
+        if (s[i] == '#'):
+            for x in range(last_hash_pos, (i-2)):
+                decrypted_string += chr(int(s[x])+96)
+            decrypted_string += chr(int(s[i-2:i])+ 96)
+            last_hash_pos = i+1
+        elif (not s[i].isdigit()):
+            return "Invalid Input"
+        else:
+            continue
+    #after going through the string, convert any digits after the last # pos to characters
+    if (last_hash_pos-1 != len(s)):
+        for i in range(last_hash_pos, len(s)):
+            decrypted_string += chr(int(s[i])+96)
+    return decrypted_string
+
+"""
+function that declares and initializes strings to be tested
+"""
+def test_freqAlphabets():
+    #initialize input strings. for testing purposes, i have created multiple valid and invalid tuples.
+    # the input string is in index 0 and the expected output is in index 1
+    test_num = 0
+    empty_string = ("","")
+    non_int_string = ("abcd","Invalid Input")
+    single_char_invalid = ('k', "Invalid Input")
+    simple_invalid_string = ("12.345", "Invalid Input")
+    mixed_invalid_string = ("1*2c4?.d", "Invalid Input")
+    simple_string = ("12345", "abcde")
+    one_letter_string = ("9", "i")
+    double_digit_letter = ("25#", "y")
+    simple_string_2 = ("10#11#12", "jkab")
+    long_string = ("12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#", "abcdefghijklmnopqrstuvwxyz")
+
+    for test_list in (empty_string,
+                      non_int_string,
+                      single_char_invalid,
+                      simple_invalid_string,
+                      mixed_invalid_string,
+                      simple_string,
+                      one_letter_string,
+                      double_digit_letter,
+                      simple_string_2,
+                      long_string):
+        test_num+=1
+        print ("================ Test ", test_num, " ================")
+        print ("Input: ", test_list[0])
+        print ("Expected: ", test_list[1])
+        print ("Output: ", freqAlphabets(test_list[0]))
+        if (test_list[1] == freqAlphabets(test_list[0])):
+            print ("PASS")
+        else:
+            print ("FAIL")
+
+if __name__ == '__main__':    
+    test_freqAlphabets()
